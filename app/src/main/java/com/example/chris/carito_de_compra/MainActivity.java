58281c2a80fe2cf2655fb3a65b3c20982966828f
abc <abc@example.com>
2018-07-16 15:53:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/chris/carito_de_compra/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.chris.carito_de_compra;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
